a23e76354be9ca1fb9ed3f08a9a9468e1260cd27
gabriel-analise <analise.gabriel@hotmail.com>
2023-03-22 14:59:00 -0300

    Atualização de imagens

--- src/main/java/numistrade/form/FormMenuPrincipal.java
+++ src/main/java/numistrade/form/FormMenuPrincipal.java
@@ -8,6 +8,15 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Window.Type;
+import javax.swing.JButton;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
+import java.awt.Cursor;
+import javax.swing.DebugGraphics;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FormMenuPrincipal extends JFrame {
 
@@ -33,19 +42,33 @@ public class FormMenuPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public FormMenuPrincipal() {
+		setTitle("Menu principal");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 668, 545);
+		setBounds(100, 100, 658, 458);
 		contentPane = new JPanel();
+		contentPane.setDebugGraphicsOptions(DebugGraphics.NONE_OPTION);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
-
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel
-				.setIcon(new ImageIcon(FormMenuPrincipal.class.getResource("/numistrade/images/NumisTradeLogo.png")));
-		lblNewLabel.setBounds(0, 0, 650, 139);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.setBounds(30, 273, 89, 23);
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.setLayout(null);
+		btnEntrar.setForeground(new Color(255, 255, 255));
+		btnEntrar.setContentAreaFilled(false);
+		btnEntrar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnEntrar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnEntrar.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(255, 255, 255)));
+		contentPane.add(btnEntrar);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(0, 0, 642, 419);
+		lblNewLabel.setIcon(new ImageIcon(FormMenuPrincipal.class.getResource("/numistrade/images/NumisMainMenu.gif")));
 		contentPane.add(lblNewLabel);
 	}
 }
