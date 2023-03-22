93f191bf73e313c05f24ac47d2426bfadbe35d2b
gabriel-analise <analise.gabriel@hotmail.com>
2023-03-22 19:39:08 -0300

    algumas mudanças e removendo o gif de carrgamento

--- src/main/java/numistrade/app/App.java
+++ src/main/java/numistrade/app/App.java
@@ -4,8 +4,9 @@ import numistrade.form.FormMenuPrincipal;
 
 public class App {
 	public static void main(String[] args) {
-		
+
 		FormMenuPrincipal formMenuPrincipal = new FormMenuPrincipal();
+		
 		formMenuPrincipal.setVisible(true);
 		
 	}
--- src/main/java/numistrade/form/FormMenuPrincipal.java
+++ src/main/java/numistrade/form/FormMenuPrincipal.java
@@ -51,7 +51,7 @@ public class FormMenuPrincipal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		setLocationRelativeTo(null);
-		
+
 		JButton btnEntrar = new JButton("Entrar");
 		btnEntrar.setBounds(30, 273, 89, 23);
 		btnEntrar.addActionListener(new ActionListener() {
@@ -59,13 +59,37 @@ public class FormMenuPrincipal extends JFrame {
 			}
 		});
 		contentPane.setLayout(null);
+
+		JButton btnCadastrarUmaNova = new JButton("Cadastrar uma nova conta");
+		btnCadastrarUmaNova.setForeground(Color.WHITE);
+		btnCadastrarUmaNova.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnCadastrarUmaNova.setContentAreaFilled(false);
+		btnCadastrarUmaNova.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(255, 255, 255)));
+		btnCadastrarUmaNova.setBounds(30, 306, 169, 23);
+		contentPane.add(btnCadastrarUmaNova);
+		/*btnCadastrarUmaNova.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FormCarregando formCarregando = new FormCarregando();
+				formCarregando.setVisible(true);
+				dispose();
+
+			}
+		});*/
+
 		btnEntrar.setForeground(new Color(255, 255, 255));
 		btnEntrar.setContentAreaFilled(false);
 		btnEntrar.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnEntrar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnEntrar.setBorder(new MatteBorder(0, 0, 1, 0, (Color) new Color(255, 255, 255)));
 		contentPane.add(btnEntrar);
-		
+
+		JLabel lblNewLabel_1 = new JLabel("Menu Principal");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(186, 11, 270, 38);
+		contentPane.add(lblNewLabel_1);
+
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setBounds(0, 0, 642, 419);
 		lblNewLabel.setIcon(new ImageIcon(FormMenuPrincipal.class.getResource("/numistrade/images/NumisMainMenu.gif")));
